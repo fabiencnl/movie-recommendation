4bf67da4ea753fed7f27871b20f9c698d385ca64
Fabien Chenal <fabienchenal@gmail.com>
2024-05-24 20:21:42 +0200

    back: improved python script

--- movie-recommendation-back/src/scripts/fetch_movies.py
+++ movie-recommendation-back/src/scripts/fetch_movies.py
@@ -1,108 +1,127 @@
 import requests
 import psycopg2
-from psycopg2 import sql
-import random
-import string
+from psycopg2 import sql, extras
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
 
-# Function to generate a random id for a given movie
-def generate_random_string(length):
-    letters = string.ascii_letters + string.digits
-    return ''.join(random.choice(letters) for _ in range(length))
+# Function to fetch the list of genres from TMDB
+def fetch_genres():
+    api_key = os.environ.get('API_KEY')
+    if not api_key:
+        return {}
 
+    url = f"https://api.themoviedb.org/3/genre/movie/list?language=en-US&api_key={api_key}"
+    response = requests.get(url)
 
-# Function to generate a random copy count for a movie
-def generate_random_copy_count():
-    return random.randint(0, 20)
+    if response.status_code == 200:
+        genres = response.json().get('genres', [])
+        return {genre['id']: genre['name'] for genre in genres}
+    else:
+        return {}
 
 
 # Function to fetch movie data from the API
 def fetch_movies(page_index):
-    # Access the API key from the environment variables
-    api_key = os.environ.get('API_TOKEN')
+    api_key = os.environ.get('API_KEY')
+    if not api_key:
+        return None
 
     try:
-        # API endpoint URL
-        url = "https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=" + str(page_index)
-
+        url = f"https://api.themoviedb.org/3/movie/top_rated?language=en-US&page={page_index}&api_key={api_key}"
         headers = {
             "accept": "application/json",
-            "Authorization": "Bearer " + api_key
         }
-
-        # Send GET request to fetch movie data
         response = requests.get(url, headers=headers)
-        # After sending the GET request
-        print("API Response:", response.text)
 
-        # Check if request was successful
         if response.status_code == 200:
             return response.json()
         else:
-            print("Failed to fetch movie data from API")
             return None
     except Exception as e:
-        print("Error fetching movie data:", e)
         return None
 
 
-# Function to insert movie data into PostgreSQL database
-def insert_movies(movies):
+# Function to insert movie and genre data into PostgreSQL database
+def insert_movies_and_genres(movies, genres):
     try:
-        # Connect to PostgreSQL database
         conn = psycopg2.connect(
-            dbname=os.environ.get('DB_NAME'),
-            user=os.environ.get('DB_USERNAME'),
-            password=os.environ.get('DB_PASSWORD'),
-            host=os.environ.get('DB_HOST')
-        )
-
-        # Create a cursor object
+             dbname=os.environ.get('DB_NAME'),
+             user=os.environ.get('DB_USERNAME'),
+             password=os.environ.get('DB_PASSWORD'),
+             host=os.environ.get('DB_HOST')
+            )
         cursor = conn.cursor()
 
-        # Iterate through each movie and insert into database
+        # Fetch existing genres to avoid duplicates
+        cursor.execute("SELECT id FROM genre")
+        existing_genres = {row[0] for row in cursor.fetchall()}
+
+        new_genres = []
+        movie_data = []
+        movie_genre_data = []
+
         for movie in movies['results']:
-            # Generate random id
-            random_id = generate_random_string(10)  # Generate a random string of length 10
+            movie_id = movie['id']
+            title = movie['title']
+            release_date = movie.get('release_date', None)
+            poster_path = movie.get('poster_path', None)
+            vote_average = movie.get('vote_average', 0.0)
+            popularity = movie.get('popularity', 0.0)
 
-            # Generate random available_copies count
-            available_copies = generate_random_copy_count()
+            if not release_date or not title:
+                continue
 
-            # Construct SQL query to insert movie
-            insert_query = sql.SQL("INSERT INTO movie (id, title, overview, release_date, poster_path, vote_average, "
-                                   "available_copies)"
-                                   " VALUES (%s, %s, %s, %s, %s, %s, %s)")
-            cursor.execute(insert_query, (random_id, movie['title'], movie['overview'], movie['release_date'],
-                                          movie['poster_path'], movie['vote_average'], available_copies))
+            movie_data.append((movie_id, title, release_date, poster_path, vote_average, popularity))
 
-        # Commit the transaction
+            for genre_id in movie['genre_ids']:
+                if genre_id not in existing_genres:
+                    genre_name = genres.get(genre_id, 'Unknown')
+                    new_genres.append((genre_id, genre_name))
+                    existing_genres.add(genre_id)
+                movie_genre_data.append((movie_id, genre_id))
+
+        # Insert new genres
+        if new_genres:
+            insert_genre_query = sql.SQL("INSERT INTO genre (id, name) VALUES %s ON CONFLICT (id) DO NOTHING")
+            extras.execute_values(cursor, insert_genre_query, new_genres)
+
+        # Insert movies
+        insert_movie_query = sql.SQL(
+            "INSERT INTO movie (id, title, release_date, poster_path, vote_average, popularity)"
+            " VALUES %s ON CONFLICT (id) DO NOTHING"
+        )
+        extras.execute_values(cursor, insert_movie_query, movie_data)
+
+        # Insert movie_genre relationships
+        insert_movie_genre_query = sql.SQL(
+            "INSERT INTO movie_genre (movie_id, genre_id) VALUES %s ON CONFLICT DO NOTHING")
+        extras.execute_values(cursor, insert_movie_genre_query, movie_genre_data)
+
         conn.commit()
-        print("Movie data inserted successfully")
 
-        # Close database connection
         cursor.close()
         conn.close()
     except Exception as e:
-        print("Error inserting movie data into database:", e)
-
-    # Main function to fetch and insert movie data
+        return None
 
 
 def main():
-    # Fetch 300 pages of movie data from the API
+    genres = fetch_genres()
+    if not genres:
+        return
+
     i = 1
-    while i < 151:
+    while i <= 471:  # Total number of pages available in TMDB for popular api fetching, 20 movies per page
         movies = fetch_movies(i)
-        # If movie data is retrieved successfully, insert into database
+        print('i : ' , i)
         if movies:
-            insert_movies(movies)
+            insert_movies_and_genres(movies, genres)
             i += 1
         else:
-            print("No movie data fetched")
+            break
 
 
 if __name__ == "__main__":
