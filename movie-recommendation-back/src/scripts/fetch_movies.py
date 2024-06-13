52e8dd8344196a3fbc85f62854376b1bf4729543
Fabien Chenal <fabienchenal@gmail.com>
2024-06-13 19:19:05 +0200

    optimized python script

--- movie-recommendation-back/src/scripts/fetch_movies.py
+++ movie-recommendation-back/src/scripts/fetch_movies.py
@@ -12,6 +12,7 @@ load_dotenv()
 def fetch_genres():
     api_key = os.environ.get('API_KEY')
     if not api_key:
+        print("API key is missing.")
         return {}
 
     url = f"https://api.themoviedb.org/3/genre/movie/list?language=en-US&api_key={api_key}"
@@ -21,32 +22,36 @@ def fetch_genres():
         genres = response.json().get('genres', [])
         return {genre['id']: genre['name'] for genre in genres}
     else:
+        print(f"Failed to fetch genres: {response.status_code}")
         return {}
 
 
 # Function to fetch movie data from the API
-def fetch_movies(page_index):
+def fetch_movies(page_index, retries=3):
     api_key = os.environ.get('API_KEY')
     if not api_key:
+        print("API key is missing.")
         return None
 
     url = f"https://api.themoviedb.org/3/movie/top_rated?language=en-US&page={page_index}&api_key={api_key}"
     headers = {"accept": "application/json"}
-    response = requests.get(url, headers=headers)
 
-    if response.status_code == 200:
-        return response.json()
-    else:
-        return None
+    for attempt in range(retries):
+        response = requests.get(url, headers=headers)
+        if response.status_code == 200:
+            return response.json()
+        else:
+            print(f"Failed to fetch movies page {page_index} (attempt {attempt + 1}): {response.status_code}")
+            sleep(0.5)
+    return None
 
 
 # Function to insert movie and genre data into PostgreSQL database
-def insert_movies_and_genres(movies, genres, cursor, existing_genres):
-    new_genres = []
+def insert_movies_and_genres(all_movies, cursor):
     movie_data = []
     movie_genre_data = []
 
-    for movie in movies['results']:
+    for movie in all_movies:
         movie_id = movie['id']
         title = movie['title']
         release_date = movie.get('release_date', None)
@@ -58,18 +63,7 @@ def insert_movies_and_genres(movies, genres, cursor, existing_genres):
             continue
 
         movie_data.append((movie_id, title, release_date, poster_path, vote_average, popularity))
-
-        for genre_id in movie['genre_ids']:
-            if genre_id not in existing_genres:
-                genre_name = genres.get(genre_id, 'Unknown')
-                new_genres.append((genre_id, genre_name))
-                existing_genres.add(genre_id)
-            movie_genre_data.append((movie_id, genre_id))
-
-    # Insert new genres
-    if new_genres:
-        insert_genre_query = sql.SQL("INSERT INTO genre (id, name) VALUES %s ON CONFLICT (id) DO NOTHING")
-        extras.execute_values(cursor, insert_genre_query, new_genres)
+        movie_genre_data.extend((movie_id, genre_id) for genre_id in movie.get('genre_ids', []))
 
     # Insert movies
     if movie_data:
@@ -86,6 +80,18 @@ def insert_movies_and_genres(movies, genres, cursor, existing_genres):
         extras.execute_values(cursor, insert_movie_genre_query, movie_genre_data)
 
 
+# Function used to insert all genres (id/name association) in the table
+def insert_genres(genres, cursor, conn):
+    # Prepare the data for insertion
+    genre_data = [(genre_id, genre_name) for genre_id, genre_name in genres.items()]
+
+    # Insert genres
+    if genre_data:
+        insert_genre_query = sql.SQL("INSERT INTO genre (id, name) VALUES %s ON CONFLICT (id) DO NOTHING")
+        extras.execute_values(cursor, insert_genre_query, genre_data)
+        conn.commit()
+
+
 def main():
     genres = fetch_genres()
     if not genres:
@@ -98,19 +104,29 @@ def main():
             password=os.environ.get('DB_PASSWORD'),
             host=os.environ.get('DB_HOST')
         )
+
         cursor = conn.cursor()
 
-        # Fetch existing genres to avoid duplicates
-        cursor.execute("SELECT id FROM genre")
-        existing_genres = {row[0] for row in cursor.fetchall()}
+        # Insert genres first
+        insert_genres(genres, cursor, conn)
 
-        for i in range(1, 472):  # Total number of pages available in TMDB for popular api fetching, 20 movies per page
+        all_movies = []
+        batch_size = 500  # Movie batch size
+
+        # Total number of pages available in TMDB for popular API fetching, 20 movies per page
+        for i in range(1, 472):
             movies = fetch_movies(i)
-            print('Fetching page:', i)
+            print(f'Fetching page: {i}')
             if movies:
-                insert_movies_and_genres(movies, genres, cursor, existing_genres)
-                conn.commit()
+                all_movies.extend(movies['results'])
                 sleep(0.2)  # To avoid hitting API rate limits
+
+                # Insert in batches to improve performance
+                if len(all_movies) >= batch_size or i == 471:
+                    print('Batch inserting movies')
+                    insert_movies_and_genres(all_movies, cursor)
+                    conn.commit()
+                    all_movies.clear()
             else:
                 break
 
